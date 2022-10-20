0959f4246200e3a7e4a2051002240fbdf342373b
black <blackmonarch124@gmail.com>
2022-10-20 22:56:20 +0900

    최종완성코드

---
+++ src/routes/About.js
@@ -0,0 +1,14 @@
+import './About.css';
+
+function About() {
+    return (
+        <div className='about__container'>
+            <span>
+                "야호"
+            </span>
+            <span>야호 아저씨</span>
+        </div>
+    )
+}
+
+export default About;
